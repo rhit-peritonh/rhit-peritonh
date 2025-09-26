785a7318f747c5365177515a682e2c106a17a257
rhit-peritonh <Peritonh@rose-hulman.edu>
2025-09-26 15:50:01 -0400

    small changes

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const indexbtn = document.getElementById('index')
 indexbtn.addEventListener('click', () => {
     window.location.href = 'index.html';
-    document.append("indexbtn");
 });
 
 const resumebtn = document.getElementById('resume')
 resumebtn.addEventListener('click', () => {
     window.location.href="resume.html"
-    document.append("resumebtn");
 });
 
 const portbtn = document.getElementById('portfolio')
 portbtn.addEventListener('click', () => {
     window.location.href="portfolio.html"
-    document.append("portbtn")
 });
