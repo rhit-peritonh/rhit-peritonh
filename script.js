08d345102de41f72585a05a670617dfbe37807ac
rhit-peritonh <Peritonh@rose-hulman.edu>
2025-09-26 16:24:03 -0400

    c

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const links = document.querySelectorAll(".topnav a");
+const indexbtn = document.getElementById('index')
+indexbtn.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
 
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", function(e) {
-            e.preventDefault(); // prevent default jump
+const resumebtn = document.getElementById('resume')
+resumebtn.addEventListener('click', () => {
+    window.location.href="resume.html"
+});
 
-            // Remove 'active' class from all links
-            for (let j = 0; j < links.length; j++) {
-                links[j].classList.remove("active");
-            }
 
-            // Add 'active' to clicked link
-            this.classList.add("active");
-
-            // Scroll to the target section smoothly
-            const sectionId = this.getAttribute("href").substring(1);
-            const section = document.getElementById(sectionId);
-
-            if (section) {
-                section.scrollIntoView({ behavior: "smooth" });
-            }
-        });
-    }
+const portbtn = document.getElementById('portfolio')
+portbtn.addEventListener('click', () => {
+    window.location.href="portfolio.html"
 });
