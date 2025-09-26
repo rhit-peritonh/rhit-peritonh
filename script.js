07d34e1df71705c071423818845f7defa4d433a0
rhit-peritonh <Peritonh@rose-hulman.edu>
2025-09-26 15:33:13 -0400

    adding js

---
+++ script.js
@@ -0,0 +1,14 @@
+const indexbtn = document.getElementById('index')
+indexbtn.addEventListener('click', () => {
+    window.location.href = 'index.html';
+});
+
+const resumebtn = document.getElementById('resume')
+resumebtn.addEventListener('click', () => {
+    window.location.href="resume.html"
+});
+
+const portbtn = document.getElementById('portfolio')
+portbtn.addEventListener('click', () => {
+    window.location.href="portfolio.html"
+});
