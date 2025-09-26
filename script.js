328ed4dc91cc80e36dac74c2ee7dd2b73e7fd6e2
rhit-peritonh <Peritonh@rose-hulman.edu>
2025-09-26 17:00:58 -0400

    c

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-const indexbtn = document.querySelector('#main');
-indexbtn.addEventListener('click', () => {
-    window.location.href = 'index.html';
+document.addEventListener('DOMContentLoaded', () => {
+    const indexbtn = document.querySelector('#main');
+    indexbtn.addEventListener('click', () => {
+        window.location.href = 'index.html';
+    });
 });
 
-const resumebtn = document.querySelector('#resume');
-resumebtn.addEventListener('click', () => {
-    window.location.href="resume.html"
+document.addEventListener('DOMContentLoaded', () => {
+    const resumebtn = document.querySelector('#resume');
+    resumebtn.addEventListener('click', () => {
+        window.location.href = 'resume.html';
+    });
 });
 
-
-const portbtn = document.querySelector('#portfolio');
-portbtn.addEventListener('click', () => {
-    window.location.href="portfolio.html"
+document.addEventListener('DOMContentLoaded', () => {
+    const portbtn = document.querySelector('#portfolio');
+    portbtn.addEventListener('click', () => {
+        window.location.href = 'portfolio.html';
+    });
 });
